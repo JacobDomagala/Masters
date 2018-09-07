f0d8fefb9a9888a8e3b25ad9fc9c874bf0f15a61
Jakub Domagala <JacobDomagala>
2018-09-07 15:13:31 +0200

    Further changes

--- Python/main.py
+++ Python/main.py
@@ -35,7 +35,7 @@ def main():
           shaggy.Stop()
           break
 
-       time.sleep(0.1)
+       #time.sleep(0.05)
 
     print("QUIT")
 
--- Python/robot.py
+++ Python/robot.py
@@ -19,7 +19,7 @@ GY80_M_DRDY = 7
 
 echo_sensors_pins = [[21, 22], [26, 23], [27, 24], [28, 29], [11, 25]]
 
-UPPER_BOUND = 50
+UPPER_BOUND = 40
 
 class Robot:
     def __init__(self):
@@ -35,7 +35,6 @@ class Robot:
 
         self.m_CycleNumber = 0
 
-        socket.setdefaulttimeout(1.0)
         self.m_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.m_Conn = 0
         self.m_Addr = 0
@@ -69,22 +68,7 @@ class Robot:
     def Cycle(self):
         self.m_CycleNumber += 1
 
-        #self.SetMove(self.m_LeftWheel, self.m_RightWheel, 0)
-
-      #   self.m_DistFrontLeft = int(min(self.UsonicReadCM(
-      #       echo_sensors_pins[0][0], echo_sensors_pins[0][1]) + 7.7, UPPER_BOUND))
-
-      #   self.m_DistRight = int(min(self.UsonicReadCM(
-      #       echo_sensors_pins[2][0], echo_sensors_pins[2][1]) + 5.75, UPPER_BOUND))
-
-      #   self.m_DistFrontRight = int(min(self.UsonicReadCM(
-      #       echo_sensors_pins[1][0], echo_sensors_pins[1][1]) + 7.7, UPPER_BOUND))
-
-      #   self.m_DistLeft = int(min(self.UsonicReadCM(
-      #       echo_sensors_pins[3][0], echo_sensors_pins[3][1]) + 5.75, UPPER_BOUND))
-
-      #   self.m_DistFront = int(min(self.UsonicReadCM(
-      #       echo_sensors_pins[4][0], echo_sensors_pins[4][1]) + 8, UPPER_BOUND))
+        self.SetMove(-self.m_LeftWheel, -self.m_RightWheel, 0)
 
         self.m_DistFrontLeft = self.UsonicReadCM(
             echo_sensors_pins[0][0], echo_sensors_pins[0][1])
@@ -108,22 +92,31 @@ class Robot:
 
     def UsonicReadCM(self, trig, echo):
         wiringpi.digitalWrite(trig, wiringpi.HIGH)
-        self.delayMicroseconds(10)
+        self.delayMicroseconds(100)
         wiringpi.digitalWrite(trig, wiringpi.LOW)
 
         startTime = micros()
         stopTime = micros()
+        timeout = micros()
 
         while wiringpi.digitalRead(echo) == wiringpi.LOW:
            startTime = micros()
+           if micros() - timeout >= 1000000:
+              print("TIMEOUT!")
+              return 0
+
+        timeout = micros()
 
         while wiringpi.digitalRead(echo) == wiringpi.HIGH:
            stopTime = micros()
+           if micros() - timeout >= 1000000:
+              print("TIMEOUT!")
+              return 0
 
         travelTime = stopTime - startTime
 
         # divide by 58.0 to get centimeters
-        return travelTime / 58.0        
+        return travelTime / 58.0
 
     def SetMove(self, l, r, save_direction):
         wiringpi.softPwmWrite(AENBL, abs(l))
@@ -162,22 +155,22 @@ class Robot:
     def Braitenberg(self):
         self.Cycle()
 
-        prox_sensors = [self.m_DistLeft, self.m_DistFrontLeft,
-                        self.m_DistFront, self.m_DistFrontRight, self.m_DistRight]
+        prox_sensors = [self.m_DistLeft, self.m_DistFrontLeft, self.m_DistFront,
+                        self.m_DistFrontRight, self.m_DistRight]
 
-        sensors = [1-(min(i, 40)/40) for i in prox_sensors]
-        wl = [8, 10, 50, -30, -9]
-        wr = [-9, -30, -50, 10, 8]
-        b = 4
+        sensors = [round(1-(min(i, 40)/40), 2) for i in prox_sensors]
+        wl = [-8, -20, 30, 4, 4]
+        wr = [4, 4, -30, -20, -8]
+        b = 2
         rightWheel = 0
         leftWheel = 0
 
         for i in range(len(sensors)):
-            leftWheel += wl[i]*sensors[i] + b
-            rightWheel += wr[i]*sensors[i] + b
+           leftWheel += wl[i]*sensors[i] + b
+           rightWheel += wr[i]*sensors[i] + b
 
-        self.m_LeftWheel = int(-(clamp(10*leftWheel, -UPPER_BOUND, UPPER_BOUND)))
-        self.m_RightWheel = int(-(clamp(10*rightWheel, -UPPER_BOUND, UPPER_BOUND)))
+        self.m_LeftWheel = int((clamp(leftWheel, -UPPER_BOUND, UPPER_BOUND)))
+        self.m_RightWheel = int((clamp(rightWheel, -UPPER_BOUND, UPPER_BOUND)))
         self.AdjustSpeed()
 
         print(sensors)
@@ -186,25 +179,27 @@ class Robot:
     def Fuzzy(self):
         self.Cycle()
 
-        sensors = [self.m_DistLeft, min(min(self.m_DistFrontLeft, self.m_DistFront), self.m_DistFrontRight), self.m_DistRight]
+        sensors = [self.m_DistLeft, min([self.m_DistFrontLeft, self.m_DistFront, self.m_DistFrontRight]), self.m_DistRight]
         intSensors = [int(clamp(i, 10, 40)) for i in sensors]
         bytesSent = self.m_Conn.send(json.dumps(intSensors).encode())
 
-        print(json.dumps(intSensors))
         print(str(bytesSent) + " bytes sent to Matlab")
 
-        rawData = self.m_Conn.recv(20).decode()
-
-        if len(rawData) == 0:
-            self.m_LeftWheel = 0 
+        rawData = json.dumps([0,0])
+        try:
+           self.m_Conn.settimeout(1)
+           rawData = self.m_Conn.recv(8).decode()
+           print("Data received! " + str(rawData))
+        except:
+            self.m_LeftWheel = 0
             self.m_RightWheel = 0
-        else:     
-            #print("Raw data: " + str(rawData))
-            decodedData = json.loads(rawData)
-            #print("After json.loads: " + str(decodedData))
+            rawData = json.dumps([0,0])
+            print("Timeout while receiving data from Matlab!")
 
-            self.m_LeftWheel = -(clamp(decodedData[0], -UPPER_BOUND, UPPER_BOUND))
-            self.m_RightWheel = -(clamp(decodedData[1], -UPPER_BOUND, UPPER_BOUND))
+        decodedData = json.loads(rawData)
+
+        self.m_LeftWheel = clamp(decodedData[0], -UPPER_BOUND, UPPER_BOUND)
+        self.m_RightWheel = clamp(decodedData[1], -UPPER_BOUND, UPPER_BOUND)
 
         print(intSensors)
         print([self.m_LeftWheel, self.m_RightWheel])
